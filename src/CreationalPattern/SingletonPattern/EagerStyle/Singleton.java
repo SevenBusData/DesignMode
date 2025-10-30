9c73bb2ef95b99581e3c5f0ff59d17c136aa335f
seven <zhangsaiwen@beisen.com>
2025-10-30 11:42:13 +0800

    feat(java): 添加饿汉式单例模式实现
    - 创建了饿汉式单例模式的类结构
    - 实现了私有构造器和静态实例初始化
    - 提供了公共静态方法获取单例对象
    - 添加启动类用于验证单例对象的唯一性- 配置IDEA检查工具支持新的Javadoc标签

---
+++ src/CreationalPattern/SingletonPattern/EagerStyle/Singleton.java
@@ -0,0 +1,18 @@
+package CreationalPattern.SingletonPattern.EagerStyle;
+
+/**
+ * @Description: 饿汉式单例模式
+ *      在类被加载的时候创建一个对象
+ */
+public class Singleton {
+    // 私有构造器，外部无法直接创建一个对应
+    private Singleton() {}
+
+    // 创建一个私有静态类对象
+    private static Singleton instance = new Singleton();
+
+    // 公共可获取对象的静态方法
+    public static Singleton getInstance() {
+        return instance;
+    }
+}
